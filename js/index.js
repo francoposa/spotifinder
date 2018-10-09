3373901aecd1db7104da639f32d4e826ac320cfd
francojposa <f2j21p94@gmail.com>
2018-10-09 13:27:27 -0500

    updating value on all sliders

--- js/index.js
+++ js/index.js
@@ -1,8 +1,46 @@
-var danceabilityRange = document.getElementById("danceabilityRange");
-var danceabilityValue = document.getElementById("danceabilityValue");
-danceabilityValue.innerHTML = danceabilityRange.value; // Display the default slider value
+function setup() {
+	var danceabilityRange = document.getElementById("danceabilityRange");
+	var danceabilityValue = document.getElementById("danceabilityValue");
+	danceabilityValue.innerHTML = danceabilityRange.value; // Display the default slider value
 
-// Update the current slider value (each time you drag the slider handle)
-danceabilityRange.oninput = function() {
-    danceabilityValue.innerHTML = this.value;
+	var energyRange = document.getElementById("energyRange");
+	var energyValue = document.getElementById("energyValue");
+	energyValue.innerHTML = energyRange.value; // Display the default slider value
+
+	var loudnessRange = document.getElementById("loudnessRange");
+	var loudnessValue = document.getElementById("loudnessValue");
+	loudnessValue.innerHTML = loudnessRange.value; // Display the default slider value
+
+	var popularityRange = document.getElementById("popularityRange");
+	var popularityValue = document.getElementById("popularityValue");
+	popularityValue.innerHTML = popularityRange.value; // Display the default slider value
+
+	var tempoRange = document.getElementById("tempoRange");
+	var tempoValue = document.getElementById("tempoValue");
+	tempoValue.innerHTML = tempoRange.value; // Display the default slider value
+
+	var valenceRange = document.getElementById("valenceRange");
+	var valenceValue = document.getElementById("valenceValue");
+	valenceValue.innerHTML = valenceRange.value; // Display the default slider value
+
+	// Update the current slider value (each time you drag the slider handle)
+	danceabilityRange.oninput = function() {
+	    danceabilityValue.innerHTML = this.value;
+	}
+	energyRange.oninput = function() {
+	    energyValue.innerHTML = this.value;
+	}
+	loudnessRange.oninput = function() {
+	    loudnessValue.innerHTML = this.value;
+	}
+	popularityRange.oninput = function() {
+	    popularityValue.innerHTML = this.value;
+	}
+	tempoRange.oninput = function() {
+	    tempoValue.innerHTML = this.value;
+	}
+	valenceRange.oninput = function() {
+	    valenceValue.innerHTML = this.value;
+	}
 }
+setup();
