273f004fb45f2aca0ab375a132dc6fc9721fc960
francojposa <f2j21p94@gmail.com>
2018-10-20 16:06:09 -0500

    whatever

--- js/index.js
+++ js/index.js
@@ -20,71 +20,73 @@ var valenceValue = document.getElementById('valenceValue');
 valenceValue.innerHTML = valenceRange.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
-danceabilityRange.oninput = function() {
+danceabilityRange.oninput = function () {
     danceabilityValue.innerHTML = this.value;
 }
-danceabilityRange.onchange = function() {
+danceabilityRange.onchange = function () {
     danceabilityValue.innerHTML = this.value;
 }
-energyRange.oninput = function() {
+energyRange.oninput = function () {
     energyValue.innerHTML = this.value;
 }
-energyRange.onchange = function() {
+energyRange.onchange = function () {
     energyValue.innerHTML = this.value;
 }
-loudnessRange.oninput = function() {
+loudnessRange.oninput = function () {
     loudnessValue.innerHTML = this.value;
 }
-loudnessRange.onchange = function() {
+loudnessRange.onchange = function () {
     loudnessValue.innerHTML = this.value;
 }
-tempoRange.oninput = function() {
+tempoRange.oninput = function () {
     tempoValue.innerHTML = this.value;
 }
-tempoRange.onchange = function() {
+tempoRange.onchange = function () {
     tempoValue.innerHTML = this.value;
 }
-valenceRange.oninput = function() {
+valenceRange.oninput = function () {
     valenceValue.innerHTML = this.value;
 }
-valenceRange.onchange = function() {
+valenceRange.onchange = function () {
     valenceValue.innerHTML = this.value;
 }
 
-document.getElementById('searchBtn').onclick = function(e) {
-	e.preventDefault();
-	analyze()
+document.getElementById('searchBtn').onclick = function (e) {
+    e.preventDefault();
+    analyze()
 }
-current_seed_uri = null
-document.addEventListener('DOMContentLoaded', function() {
-    current_seed_uri = parse_uri(document.getElementById('seedObject').data)
-    set_sliders({
-        'danceability': 74,
-        'energy': 76,
-        'loudness': 80,
-        'tempo': 117,
-        'valence': 71
-    })
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     if (!('hasCodeRunBefore' in localStorage)) {
+//         set_sliders({
+//             'danceability': 74,
+//             'energy': 76,
+//             'loudness': 88,
+//             'tempo': 117,
+//             'valence': 71
+//         });
+//         localStorage.setItem("hasCodeRunBefore", true);
+//     }
+// });
 
 const LIMIT = 12
 
 
 function analyze() {
-	raw_input = document.getElementById('searchInput').value
-	uri = parse_uri(raw_input)	
-	update_embed_object(uri, 'seedObject')
-	do_get_analysis(uri)
+    console.log("calling analyze");
+    raw_input = document.getElementById('searchInput').value;
+    uri = parse_uri(raw_input);
+    console.log("about to update embed object");
+    update_embed_object(uri, 'seedObject');
+    console.log("about to get analysis")
+    do_get_analysis(uri);
 }
 
 function parse_uri(raw_input) {
-	url_without_query_string = raw_input.split(/[?#]/)[0];
-	url_no_trailing_slash = url_without_query_string.endsWith('/') ? url_without_query_string.slice(0, -1) : url_without_query_string;
-	document.getElementById('searchInput').value = url_no_trailing_slash;
-	url_sections = url_no_trailing_slash.split('/');
-	uri = url_sections[url_sections.length - 1];
-	console.log(uri);
-	return uri
+    url_without_query_string = raw_input.split(/[?#]/)[0];
+    url_no_trailing_slash = url_without_query_string.endsWith('/') ? url_without_query_string.slice(0, -1) : url_without_query_string;
+    url_sections = url_no_trailing_slash.split('/');
+    uri = url_sections[url_sections.length - 1];
+    return uri
 }
 
 function update_embed_object(new_uri, id_to_update) {
@@ -92,54 +94,65 @@ function update_embed_object(new_uri, id_to_update) {
     current_uri = parse_uri(obj.data);
     if (current_uri == new_uri) {
         return;
+    } else {
+        embed_url = data = "https://open.spotify.com/embed/track/" + new_uri;
+        obj.outerHTML = obj.outerHTML.replace(/data="(.+?)"/, 'data="' + embed_url + '"');
     }
-	embed_url = data="https://open.spotify.com/embed/track/" + new_uri;
-	obj.outerHTML = obj.outerHTML.replace(/data="(.+?)"/, 'data="' + embed_url + '"');
 }
 
 function do_get_analysis(uri) {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', 'https://spotifinder-backend.herokuapp.com/analyze?spotify_uri=' + uri);
-	xhr.setRequestHeader('Content-Type', 'application/json');
-	xhr.onload = handle_analysis_response;
-	xhr.send();
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://spotifinder-backend.herokuapp.com/analyze?spotify_uri=' + uri);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = handle_analysis_response;
+    xhr.send();
 }
 
 function handle_analysis_response() {
-	if (this.status === 200) {
-        	analysis = JSON.parse(this.responseText);
-            do_get_recommendation(analysis)
-        	analysis = normalize_analysis(analysis)
-        	set_sliders(analysis)
-    	}
+    if (this.status === 200) {
+        raw_analysis = JSON.parse(this.responseText);
+        console.log(raw_analysis);
+        normalized_analysis = normalize_analysis(raw_analysis);
+        set_sliders(normalized_analysis);
+        do_get_recommendation(raw_analysis);
+    }
 }
 
 function normalize_analysis(raw_analysis) {
-	danceability = Math.round(analysis['danceability'] * 100)
-	energy = Math.round(analysis['energy'] * 100)
-	loudness = Math.round(((-60 - analysis['loudness']) / -60) * 100)
-	tempo = Math.round(analysis['tempo'])
-	valence = Math.round(analysis['valence'] * 100)
-	return {
-		'danceability': danceability,
-		'energy': energy,
-		'loudness': loudness,
-		'tempo': tempo,
-		'valence': valence
-	}
+    console.log("normalizing analysis: " + raw_analysis);
+    danceability = Math.round(raw_analysis['danceability'] * 100);
+    energy = Math.round(raw_analysis['energy'] * 100);
+    loudness = Math.round(((-60 - raw_analysis['loudness']) / -60) * 100);
+    tempo = Math.round(raw_analysis['tempo']);
+    valence = Math.round(raw_analysis['valence'] * 100);
+    console.log({
+        'danceability': danceability,
+        'energy': energy,
+        'loudness': loudness,
+        'tempo': tempo,
+        'valence': valence
+    });
+    return {
+        'danceability': danceability,
+        'energy': energy,
+        'loudness': loudness,
+        'tempo': tempo,
+        'valence': valence
+    };
 }
 
 function set_sliders(normalized_analysis) {
-	danceabilityRange.value = normalized_analysis['danceability']
-	danceabilityRange.onchange()
-	energyRange.value = normalized_analysis['energy']
-	energyRange.onchange()
-	loudnessRange.value = normalized_analysis['loudness']
-	loudnessRange.onchange()
-	tempoRange.value = normalized_analysis['tempo']
-	tempoRange.onchange()
-	valenceRange.value = normalized_analysis['valence']
-	valenceRange.onchange()
+    console.log("setting sliders");
+    danceabilityRange.value = normalized_analysis['danceability'];
+    danceabilityRange.onchange();
+    energyRange.value = normalized_analysis['energy'];
+    energyRange.onchange();
+    loudnessRange.value = normalized_analysis['loudness'];
+    loudnessRange.onchange();
+    tempoRange.value = normalized_analysis['tempo'];
+    tempoRange.onchange();
+    valenceRange.value = normalized_analysis['valence'];
+    valenceRange.onchange();
 }
 
 function do_get_recommendation(raw_analysis) {
@@ -172,11 +185,11 @@ function prep_recommendation_params(raw_analysis) {
 
 function handle_recommendation_response() {
     if (this.status === 200) {
-            recommendations = JSON.parse(this.responseText);
-            console.log(recommendations)
-            update_recommendations(recommendations)
+        recommendations = JSON.parse(this.responseText);
+        console.log(recommendations)
+        update_recommendations(recommendations)
 
-        }
+    }
 }
 
 function update_recommendations(raw_recommendations) {
